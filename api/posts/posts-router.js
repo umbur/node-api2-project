eb6872a0dc580602c5f5f4dc9638c92a2f7217c4
umbur <work1415@gmail.com>
2021-05-04 15:42:54 -0600

    reviewing node/express/crud/db

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,83 @@
 // implement your posts router here
+const express = require("express");
+const posts = require("./posts-model");
+
+const router = express.Router();
+
+router.get("/api/posts", (req, res) => {
+  posts
+    .find(posts)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving data",
+      });
+    });
+});
+
+router.get("/api/posts/:id", (req, res) => {
+  posts
+    .findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "Post not found!" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the post",
+      });
+    });
+});
+
+router.post("/api/posts", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    return res.status(400).json({
+      message: "Please provide values for title and contents!",
+    });
+  }
+  posts
+    .insert(req.body)
+    .then((post) => {
+      res.status(201).json({ message: `The post ${post} has been added!` });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        meesage: "Error adding the post to DB!",
+      });
+    });
+});
+
+router.put("/api/posts/:id", (req, res) => {
+  const changes = req.body;
+  const id = req.params.id;
+  posts
+    .update(id, changes)
+    .then((changes) => {
+      if (changes) {
+        res.status(200).json(changes);
+      } else {
+        res.status(404).json({ message: "The post could not be found!" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error updating post!",
+      });
+    });
+});
+
+router.delete("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+  posts.remove(id).then();
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,12 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+
+const server = express();
+
+const postsRouter = require("./posts/posts-router");
+
+server.use(express.json());
+server.use(postsRouter);
+
+module.exports = server;

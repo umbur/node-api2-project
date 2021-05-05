f676f96dad09f62953220fafdb537e7714af6f64
umbur <work1415@gmail.com>
2021-05-04 22:50:46 -0600

    added react client with ui to interact with db with crud operations

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -58,13 +58,18 @@ router.post("/api/posts", (req, res) => {
 router.put("/api/posts/:id", (req, res) => {
   const changes = req.body;
   const id = req.params.id;
+
   posts
     .update(id, changes)
-    .then((changes) => {
-      if (changes) {
-        res.status(200).json(changes);
+    .then((post) => {
+      if (!changes.title || !changes.contents) {
+        res.status(400).json({
+          message: "Please provide title and content!",
+        });
       } else {
-        res.status(404).json({ message: "The post could not be found!" });
+        res
+          .status(200)
+          .json({ message: `The post id#${req.params.id} has been updated!` });
       }
     })
     .catch((error) => {
@@ -77,7 +82,35 @@ router.put("/api/posts/:id", (req, res) => {
 
 router.delete("/api/posts/:id", (req, res) => {
   const id = req.params.id;
-  posts.remove(id).then();
+  posts
+    .remove(id)
+    .then((post) => {
+      res.status(200).json({
+        message: `The post with the id of ${req.params.id} has been removed!`,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "The post can not be removed!",
+      });
+    });
+});
+
+router.get("/api/posts/:id/comments", (req, res) => {
+  const id = req.params.id;
+
+  posts
+    .findPostComments(id)
+    .then((comments) => {
+      res.status(200).json(comments);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "The comments information could not be retrieved",
+      });
+    });
 });
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require("express");
+const cors = require("cors");
 
 const server = express();
 
 const postsRouter = require("./posts/posts-router");
 
 server.use(express.json());
+server.use(cors());
 server.use(postsRouter);
 
 module.exports = server;
